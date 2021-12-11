624dc56e4f07c1bc11d27da0403a8cb757155c85
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2021-12-11 15:50:44 +0200

    initial project structure created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+import Modal from "./components/Modal/Modal";
+
+class App extends Component {
+  state = { showModal: true };
+
+  ////////// Methods
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  ////////// END Methods
+  render() {
+    const { showModal } = this.state;
+    return (
+      <div className="App">
+        This is hw 3!;
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <h1>Hi from JS</h1>
+          </Modal>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { createPortal } from "react-dom";
+import "./Modal.css";
+
+const modalRoot = document.querySelector("#modal-root");
+
+export default class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeydown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeydown);
+  }
+
+  handleKeydown = (e) => {
+    if (e.code === "Escape") {
+      this.props.onClose();
+    }
+  };
+
+  handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className="Overlay" onClick={this.handleOverlayClick}>
+        <div className="Modal">{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
+}
